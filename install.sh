c77acc8cee4649cc22731a34e1f3b1cab5b394ce
Alexandru Danila <alex.danila94@gmail.com>
2021-04-03 21:41:23 -0400

    Add: Moderate overhaul of install script
    Moved creation of the temp directory before the formatting of the
    storage since formatting should not occur if temp directory cannot be
    made.
    
    Added ability to select between 32 bit (armv7l) and 64 bit (AArch64)
    versions with default to 32 bit and instructions for making the
    selection.
    
    Added instructions to the end of the script. If installing 64 bit, it
    also displays a message about the known USB issue on the 8 GB Pi 4 and
    the workaround.
    
    Installer now changes to the temp directory once it is set up. Still
    uses absolute paths for everything, but the change is done to enable
    md5sum.
    
    Installer now downloads the .md5 of the tarball and verifies before
    installing.

--- install.sh
+++ install.sh
@@ -5,7 +5,18 @@
 # Should also work with Pi 3.
 
 function usage() {
-    echo Usage: $0 /dev/sdX
+    echo
+    echo Usage: $0 /dev/sdX [architecture]
+    echo
+    echo ARCHITECTURES
+    echo "    armv7, armv7l, arm32"
+    echo "      Treated as synonymous for armv7l for the Pi 3 and Pi 4."
+    echo "      Specifically, ArchLinuxARM-rpi-4-latest, which runs on both."
+    echo
+    echo "    aarch64, arm64"
+    echo "      Treated as synonymous for AArch64 for the Pi 3 and Pi 4."
+    echo "      Specifically, ArchLinuxARM-rpi-aarch64-latest."
+
     exit $1
 }
 
@@ -16,61 +27,91 @@ function verify_exit() {
     fi
 }
 
-if [[ $# -ne 1 ]]; then
+
+if [[ `id -u` -ne 0 ]]; then
+    echo "This script needs root permssions to interact with block devices. Please run as root."
+    exit 0
+fi
+
+if [[ $# -lt 1  || $# -gt 2 ]]; then
     echo Invalid number of arguments
     usage 1
 fi
 if [[ $1 != /dev/sd* ]]; then
-    echo Invalid device: $1
+    echo Invalid device: \'$1\'
     usage 2
 fi
 if [[ ! -e $1 ]]; then
-    echo Device not found: $1
+    echo Device not found: \'$1\'
     usage 3
 fi
 
-if [[ `id -u` -ne 0 ]]; then
-    echo "This script needs root permssions to interact with block devices. Please run as root."
-    exit 4
+
+# Select a target URL based on architecture
+# Contrary to the name, the rpi-4 tarball works on the Raspberry Pi 3
+URL=http://os.archlinuxarm.org/os
+TARBALL=ArchLinuxARM-rpi-4-latest.tar.gz
+ARCH=armv7l
+if [[ $# -eq 2 ]]; then
+    case $2 in
+        armv7 | armv7l | arm32)
+            URL=http://os.archlinuxarm.org/os
+            TARBALL=ArchLinuxARM-rpi-4-latest.tar.gz
+            ARCH=armv7l
+            ;;
+        aarch64 | arm64)
+            URL=http://os.archlinuxarm.org/os
+            TARBALL=ArchLinuxARM-rpi-aarch64-latest.tar.gz
+            ARCH=aarch64
+            ;;
+        *)
+            echo Invalid architecture \'$2\'
+            usage 4
+            ;;
+    esac
+else
+    echo Assuming 32 bit Arm
 fi
-
-TGTDEV=$1
-echo Partitioning $1...
-sfdisk $TGTDEV < alarm.sfdisk
-verify_exit $? "Partitioning $TGTDEV failed" -1
-
-BOOTPART=$11
-echo Formatting $BOOTPART as vfat...
-yes | mkfs.vfat $BOOTPART -n boot
-verify_exit $? "Formatting $BOOTPART failed" -2
-
-ROOTPART=$12
-echo Formatting $ROOTPART as ext4...
-yes | mkfs.ext4 $ROOTPART -L root
-verify_exit $? "Formattinging $ROOTPART failed" -2
 
 
 # Paranoid amount of verifying the temp directory,
 # but if it ends up being in the wrong place we could
-# end up woring in /
+# end up ruining a directory. If $TMP is blank we could
+# end up in /
 echo Setting up temporary directory...
 TMP=`mktemp -d`
 if [[ ! -d $TMP ]]; then
     echo $TMP does not exist
-    exit -4
+    exit -1
 fi
 if [[ $TMP/ != /tmp/* ]]; then
     echo $TMP not in /tmp
-    exit -5
+    exit -2
 fi
 if find $TMP -mindepth 1 | read; then
     echo $TMP not empty
-    exit -6
+    exit -3
 fi
 BOOT=$TMP/boot
 ROOT=$TMP/root
 mkdir $BOOT
 mkdir $ROOT
+
+
+TGTDEV=$1
+echo Partitioning $1...
+sfdisk $TGTDEV < alarm.sfdisk
+verify_exit $? "Partitioning $TGTDEV failed" -4
+
+BOOTPART=$11
+echo Formatting $BOOTPART as vfat...
+yes | mkfs.vfat $BOOTPART -n boot
+verify_exit $? "Formatting $BOOTPART failed" -5
+
+ROOTPART=$12
+echo Formatting $ROOTPART as ext4...
+yes | mkfs.ext4 $ROOTPART -L root
+verify_exit $? "Formattinging $ROOTPART failed" -6
 
 
 echo Mounting $BOOTPART to $BOOT...
@@ -81,40 +122,76 @@ echo Mounting $ROOTPART to $ROOT...
 mount $ROOTPART $ROOT
 verify_exit $? "Mounting $ROOTPART failed" -8
 
-echo Downloading http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz...
-curl -L http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz > $TMP/alarm.tar.gz
-verify_exit $? "Downloading tarball failed" -9
+echo Entering $TMP...
+pushd $TMP > /dev/null 2>&1
+verify_exit $? "Entering $TMP failed" -10
 
-echo Extracting $TMP/alarm.tar.gz...
-bsdtar -xpf $TMP/alarm.tar.gz -C $ROOT
-verify_exit $? "Extracting tarball failed" -10
+
+echo Downloading $TARBALL...
+curl -OL $URL/$TARBALL --output-dir $TMP
+verify_exit $? "Downloading $TARBALL failed" -11
+
+echo Downloading $TARBALL.md5...
+curl -OL $URL/$TARBALL.md5 --output-dir $TMP
+verify_exit $? "Downloading $TARBALL.md5 failed" -12
+
+echo Verifying $TARBALL...
+md5sum --check $TARBALL.md5
+verify_exit $? "Verifying $TARBALL failed" -13
+
+
+echo Extracting $TARBALL...
+bsdtar -xpf $TMP/$TARBALL -C $ROOT
+verify_exit $? "Extracting tarball failed" -14
 mv $TMP/root/boot/* $BOOT
+
 
 echo Updating fstab...
 sed -i 's/\/dev\/mmcblk0p1/LABEL=boot/g' $ROOT/etc/fstab
-verify_exit $? "Updating fstab failed" -11
+verify_exit $? "Updating fstab failed" -15
+
 
 echo Synchronizing partitions...
 sync
 
+
 echo Unmounting partitions...
 umount $BOOT $ROOT
-verify_exit $? "Failed to umount partitions" -12
+verify_exit $? "Failed to umount partitions" -16
+
 
 # Verify temp directory again because we're about to
 # rm -r $TMP and we definitely don't want to accidentally
 # rm some random directory (especially if that directory
 # is /)
 echo Cleaning up...
+popd > /dev/null 2>&1
 if [[ ! -d $TMP ]]; then
     echo $TMP does not exist
-    exit -13
+    exit -17
 fi
 if [[ $TMP/ != /tmp/* ]]; then
     echo $TMP not in /tmp
-    exit -14
+    exit -18
 fi
 rm -r $TMP
 
+
+# Instructions and notes
+if [[ $ARCH = aarch64 ]]; then
+    echo
+    echo Note: As of 3 April 2021, the AArch64 kernel does not properly initialize
+    echo USB on the 8GB Pi 4. Until this is fixed, the workaround is to install the
+    echo \`linux-raspberrypi4\` kernel. Contrary to the name, this kernel also works
+    echo on the Pi 3. Use either SSH or a Pi 3/Pi 4 with less than 8GB of RAM to do
+    echo the install, as you will not have a working keyboard until you do.
+    echo
+fi
+echo Default user $(tput bold)alarm$(tput sgr0) with password $(tput bold)alarm$(tput sgr0).
+echo Default $(tput bold)root$(tput sgr0) password is $(tput bold)root$(tput sgr0).
+echo Remember to run \`pacman-key --init\` and \`pacman-key --populate archlinuxarm\`
+
+
+echo
 echo Done
 
